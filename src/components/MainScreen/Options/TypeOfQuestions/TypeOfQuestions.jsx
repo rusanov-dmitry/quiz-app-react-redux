3cc858eb22589332b53259427266db79c67bc84b
rusanov-dmitry <65367402+rusanov-dmitry@users.noreply.github.com>
2021-07-13 00:22:07 +0600

    Create radio button component

--- src/components/MainScreen/Options/TypeOfQuestions/TypeOfQuestions.jsx
+++ src/components/MainScreen/Options/TypeOfQuestions/TypeOfQuestions.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './TypeOfQuestions.module.css';
+import RadioInput from './../../../RadioInput/RadioInput';
 
 const TypeOfQuestions = (props) => {
     return (
         <div className={styles.typeOfQuestions}>
-
+            <h2>Answers type</h2>
+            <RadioInput typeName='Any' />
+            <RadioInput typeName='Multiple Choice' />
+            <RadioInput typeName='True / False' />
         </div>
     )
 }
--- src/components/RadioInput/RadioInput.jsx
+++ src/components/RadioInput/RadioInput.jsx
@@ -5,7 +5,7 @@ const RadioInput = (props) => {
     return (
         <div className={styles.radioButton}>
             <input type="radio" id='radio1' name='opop' value='any' className={styles.customRadio}/>
-            <label htmlFor="radio1">Option 1</label>
+            <label htmlFor="radio1">{props.typeName}</label>
         </div>
     )
 }
