e82923e36cedf598c273cda68b9d5690b71d1efe
rusanov-dmitry <65367402+rusanov-dmitry@users.noreply.github.com>
2021-07-31 03:31:03 +0600

    Add non-existing quiz handling

--- src/components/QuizScreen/QuizScreen.jsx
+++ src/components/QuizScreen/QuizScreen.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Question from './Question/Question';
 import styles from './QuizScreen.module.css';
+import WrongQuizHandler from './WrongQuizHandler/WrongQuizHandler';
 
 const QuizScreen = (props) => {
     return (
         <div className={styles.content}>
-            <h2>{typeof props.questions[0] !== 'undefined' ? props.questions[0].category : 'Loading...'}</h2>
+            <h2>{typeof props.questions[0] !== 'undefined' ? props.questions[0].category : <WrongQuizHandler />}</h2>
         </div>
     )   
 }
---
+++ src/components/QuizScreen/WrongQuizHandler/WrongQuizHandler.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './WrongQuizHandler.module.css';
+import PrimaryButton from '../../PrimaryButton/PrimaryButton';
+
+const WrongQuizHandler = (props) => {
+    return (
+        <div className={styles.content}>
+            <h2>Sorry, something went wrong :(<br /> Try another quiz!</h2>
+            <PrimaryButton itemPath='/main' itemName='Got it!' />
+        </div>
+    )
+}
+
+export default WrongQuizHandler;
